7e8fea18fec7a05d4a738703b2b959d3cbbc6122
ProfessorBuny <nikunjraghav24@gmail.com>
2025-02-10 23:50:41 +0000

    adding one dumy data

--- src/index.js
+++ src/index.js
@@ -13,21 +13,26 @@ const Books = () => {
   return (
     <section>
       <Title />
-      <Author />
       <Image />
+      <Author />
     </section>
   );
 };
 
 const Image = () => {
-  return <img src="" alt="" />;
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/41OPosUIEhL._SY445_SX342_.jpg"
+      alt=""
+    />
+  );
 };
 
 const Title = () => {
-  return <h1>title</h1>;
+  return <h1>INNER EXCELLENCE</h1>;
 };
 
-const Author = () => <h4>author</h4>;
+const Author = () => <h4>JIM MURPHY </h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
