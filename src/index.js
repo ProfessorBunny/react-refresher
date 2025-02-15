3c79cff7c837655021fdbc6e31b606edbd926cca
ProfessorBuny <nikunjraghav24@gmail.com>
2025-02-15 23:14:15 +0000

    js in jsx

--- src/index.js
+++ src/index.js
@@ -15,16 +15,16 @@ function Booklist() {
 const author = "Jordan Moore";
 
 const Books = () => {
-  const title = "Interesting Facts For Curious Mindssssss";
+  const title = "INNER EXCELLENCE";
 
   return (
     <>
-      <h1>INNER EXCELLENCE</h1>;
+      <h1>{title}</h1>;
       <img
         src="https://m.media-amazon.com/images/I/41OPosUIEhL._SY445_SX342_.jpg"
         alt=""
       />
-      <h4 style={{ fontSize: "0.75rem" }}>JIM MURPHY </h4>;
+      <h4 style={{ fontSize: "0.75rem" }}>{author} </h4>;
     </>
   );
 };
