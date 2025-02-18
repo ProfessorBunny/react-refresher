e9fc390b51b541c8a05bc88f53013580f4b268c7
ProfessorBuny <nikunjraghav24@gmail.com>
2025-02-18 07:21:57 +0000

    proper list

--- src/index.js
+++ src/index.js
@@ -3,30 +3,29 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  author: "Jordan Moore",
-  title: "Interesting Facts For Curious Minds",
-  img: "https://m.media-amazon.com/images/I/41OPosUIEhL._SY445_SX342_.jpg",
-};
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://m.media-amazon.com/images/I/51-y8++XilL._SX342_SY445_.jpg",
-};
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "https://m.media-amazon.com/images/I/41OPosUIEhL._SY445_SX342_.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/51-y8++XilL._SX342_SY445_.jpg",
+    id: 2,
+  },
+];
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        console.log(book);
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
     </section>
   );
 }
