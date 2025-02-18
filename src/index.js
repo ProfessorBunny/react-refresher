2589c8410b9041e5a33c25a99aa120dc3eeaea94
ProfessorBuny <nikunjraghav24@gmail.com>
2025-02-18 06:09:40 +0000

    using props

--- src/index.js
+++ src/index.js
@@ -10,19 +10,18 @@ const img = "https://m.media-amazon.com/images/I/41OPosUIEhL._SY445_SX342_.jpg";
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book title={title} img={img} />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author} </h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author} </h4>
     </article>
   );
 };
