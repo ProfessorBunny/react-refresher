85082a68bd05ca52d202efaafe77ecb08c1a52f9
ProfessorBuny <nikunjraghav24@gmail.com>
2025-03-27 16:21:43 +0000

    fixing bug with wrapper function

--- src/index.js
+++ src/index.js
@@ -77,11 +77,15 @@ function Booklist() {
 
 const Book = ({ img, title, author, getBooks, id }) => {
   // const { img, title, author } = props;
+
+  const clickHandler = (e) => {
+    getBooks(id);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={getBooks(id)}>Display Title</button>
+      <button onClick={clickHandler}>Display Title</button>
       <h4>{author} </h4>
     </article>
   );
